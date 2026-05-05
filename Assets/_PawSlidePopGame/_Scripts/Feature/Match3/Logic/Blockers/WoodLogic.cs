be6c12483404b239c389890c1cec7afa5ca439d8
Maz2605 <vannam2654@gmail.com>
2026-05-05 23:40:01 +0700

    Add chocolate cake and stone mechanics

---
+++ Assets/_PawSlidePopGame/_Scripts/Feature/Match3/Logic/Blockers/WoodLogic.cs
@@ -0,0 +1,15 @@
+﻿using _PawSlidePopGame._Scripts.Feature.Match3.Logic.Base;
+using _PawSlidePopGame._Scripts.Feature.Match3.Model.Board;
+using _PawSlidePopGame._Scripts.Feature.Match3.Model.Entities;
+using _PawSlidePopGame._Scripts.Feature.Match3.Presentation;
+
+namespace _PawSlidePopGame._Scripts.Feature.Match3.Logic.Blockers
+{
+    public class WoodLogic : BaseBlockerLogic
+    {
+        public override void OnExploded(BoardModel board, CellModel cell, TileModel tile, BoardFxContext fxContext)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
